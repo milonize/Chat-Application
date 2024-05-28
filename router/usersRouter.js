3f4d00e48bce24519cd98c882ebe0c484d405d65
MiloniZe <milonizehere@gmail.com>
2024-05-28 14:56:11 +0600

    push

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,5 +1,5 @@
 const express= require('express')
-const {getUsersPage,createUser}= require('../controller/users')
+const {getUsersPage,createUser, addUser}= require('../controller/users')
 const HtmlResponse= require('../middliewares/htmlResponse')
 const imagesUpload = require('../middliewares/uploadSystem/imagesUpload')
 const { addUserFormValidator, formValidatorErrors } = require('../middliewares/formHandle/formValidator')
@@ -7,6 +7,6 @@ const { addUserFormValidator, formValidatorErrors } = require('../middliewares/f
 const router = express.Router()
 
 router.get("/",HtmlResponse('Users'),getUsersPage)
-router.post("/create", imagesUpload,addUserFormValidator,formValidatorErrors)
+router.post("/create", imagesUpload,addUserFormValidator,formValidatorErrors,addUser)
 
 module.exports=router;
